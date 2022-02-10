4e271c180b428e968a75f10a2bc27aa46f2f3cb5
Andre Gonzales <andreg-16@hotmail.com>
2022-02-09 21:40:13 -0500

    Database url

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 const app = require("./app");
-
-mongoose.connect('mongodb://localhost/medicaldb')
+ 
+if (process.env.NODE_ENV === 'production') {
+    mongoose.connect(process.env.MONGODB_URI)
+        .then(() => console.log('Database is connected'))
+        .catch(console.error);
+} else {
+    mongoose.connect('mongodb://localhost/medicaldb')
     .then(() => console.log('Database is connected'))
     .catch(console.error);
+}
 
-app.listen(5000, () => {
-    console.log('Server listening on port 5000');
+const port = process.env.PORT || 5000;
+
+app.listen(port , () => {
+    console.log('Server listening on port', port);
 });
